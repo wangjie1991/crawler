2b571684d45e151e462771adf2b35d6e96920d3e
wangjie1991 <mononogy@sina.cn>
2015-09-19 15:53:41 +0800

    corpus

--- crawler/spiders/filters.py
+++ crawler/spiders/filters.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 from pybloomfilter import BloomFilter
 
 
-class NewsBloomFilter():
+class UrlFilter():
     
     def __init__(self, domain):
         self.index_tmp = '/tmp/%s_%s' % (domain, 'index_url.bloom')
@@ -33,3 +34,46 @@ class NewsBloomFilter():
             if not self.html_bf.add(link.url):
                 new_links.append(link)
         return new_links
+
+    def filter_txweibo(self, links):
+        new_links = []
+        
+        for link in links:
+            url = link.url
+            idx = url.find('?')
+            if idx != -1:
+                head = url[:idx+1]
+                tail = ''
+                tail_list = re.findall('&(pi=[0-9]*?)&', url)
+                if tail_list:
+                    tail = tail_list[0]
+                url = head + tail
+            
+            #s = '!!!!! url = ' + url
+            #log.msg(s, level=log.INFO)
+            #http://t.qq.com/kaifulee?pi=98
+            if not self.html_bf.add(url):
+                new_links.append(link)
+        
+        return new_links
+    
+    def filter_sinaweibo(self, urls):
+        new_urls = set()
+        
+        for url in urls:
+            idx = url.find('?')
+            if idx != -1:
+                head = url[:idx+1]
+                tail = ''
+                tail_list = re.findall('(page=\d*)', url)
+                if tail_list:
+                    tail = tail_list[0]
+                url = head + tail
+            
+            #s = '!!!!! url = ' + url
+            #log.msg(s, level=log.INFO)
+            #http://t.qq.com/kaifulee?page=98
+            if not self.html_bf.add(url):
+                new_urls.add(url)
+        
+        return new_urls
---
+++ crawler/spiders/sina_spider.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from news_crawler.spiders.filters import NewsBloomFilter
+from news_crawler.items import NewsItem, NewsLoader
+
+
+class NewsSinaSpider(CrawlSpider):
+    name = 'news.sina'
+    allowed_domains = ['news.sina.com.cn']
+    start_urls = ['http://news.sina.com.cn']
+    nbf = NewsBloomFilter('news_sina')
+    
+    rules = [# 规则1匹配文本页面，一定做URL去重
+             Rule(LinkExtractor(allow=(r'http://news.sina.com.cn/.*?s?html')), 
+                  callback='parse_item', follow=True, process_links=nbf.filter_html),
+             # 规则2匹配新闻索引页面，不提取文本。
+             Rule(LinkExtractor(allow=(r'http://news.sina.com.cn/.*?/')), process_links=nbf.filter_index)]
+             #Rule(LinkExtractor(allow=(r'http://news.sina.com.cn/.*?/')))]
+    
+    def parse_item(self, response):
+        loader = NewsLoader(item=NewsItem(), response=response)
+        loader.add_value('url', response.url)
+
+        # title compatible of new and old page
+        # loader.add_xpath('title', '//h1[contains(@id, "artibodyTitle")]//text()')
+        h1 = response.xpath('//h1/text()')
+        if h1:
+            loader.add_xpath('title', '//h1/text()')
+        else:
+            loader.add_xpath('title', '//th[contains(@class, "f24")]/text()')
+        
+        # text compatible of new and old page
+        t1 = response.xpath('//div[contains(@id, "artibody")]//p//text()')
+        t2 = response.xpath('//div[contains(@id, "article")]//p/text()')
+        if t1:
+            # <p>中会有<strong>修饰文本，也为了最大匹配artibody，保证<p>匹配成功
+            loader.add_xpath('text', '//div[contains(@id, "artibody")]//p//text()')
+        elif t2:
+            # <div id=article>内一些<p>会在里层，而且这里匹配失败会全匹配<p>，容易引入js杂料，所以这里需要匹配最大化
+            # 早期网页中一般没有将重要文本放入<p>里层，里层反而有js等。
+            loader.add_xpath('text', '//div[contains(@id, "article")]//p/text()')
+        else:
+            # 旧的网页对<p>敏感度高，一般不会里层包含文本。如果深层获取，还会引入一些js杂料
+            loader.add_xpath('text', '//p/text()')
+        
+        return loader.load_item()
