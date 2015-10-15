f704b7e85397b2da997c9302e9774c1304f3b8d4
wangjie1991 <mononogy@sina.cn>
2015-10-15 15:58:26 +0800

    corpus

--- crawler/spiders/qq_spider.py
+++ crawler/spiders/qq_spider.py
@@ -99,11 +99,11 @@ class QQSpider(CrawlSpider):
         path = self.pathextractor.host(settings.QQ_STORE, response.url)
         loader.add_value('path', path)
         loader.add_xpath('title', '//h1/text()')
-        loader.add_xpath('//div[contains(@class, "daoyu")]//div[contains(@class, "intr")]/text()')
-        loader.add_xpath('//div[contains(@id, "articleContent")]/h2/text()')
-        loader.add_xpath('//div[contains(@id, "articleContent")]/h3/text()')
-        loader.add_xpath('//div[contains(@id, "articleContent")]/p/text()')
-        loader.add_xpath('//div[contains(@class, "jieyu")]//text()')
+        loader.add_xpath('text', '//div[contains(@class, "daoyu")]//div[contains(@class, "intr")]/text()')
+        loader.add_xpath('text', '//div[contains(@id, "articleContent")]/h2/text()')
+        loader.add_xpath('text', '//div[contains(@id, "articleContent")]/h3/text()')
+        loader.add_xpath('text', '//div[contains(@id, "articleContent")]/p/text()')
+        loader.add_xpath('text', '//div[contains(@class, "jieyu")]//text()')
 
         return loader.load_item()
 
