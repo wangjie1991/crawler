46c4563d17159406b1e824d3595aa25091f45911
wangjie1991 <mononogy@sina.cn>
2015-10-21 11:54:39 +0800

    corpus

---
+++ text.git.cmd
@@ -0,0 +1,34 @@
+git account:
+name:wangjie1991
+mail:mononogy@sina.cn
+pwd:xyz14142718/amethyst
+
+
+git init
+git add
+git commit -m ""
+
+git status
+git diff
+
+git log
+git log --pretty=oneline
+git reset --hard HEAD^
+git reset --hard 3628164
+git reflog
+
+
+git checkout -- file
+git reset HEAD file -> git checkout -- file
+
+
+rm readme.txt
+git rm readme.txt
+git commit -m "remove readme.txt"
+
+
+git remote add origin git@github.com:wangjie1991/learngit.git
+git push -u origin master
+git push origin master
+git clone git@github.com:wangjie1991/gitskills.git
+
